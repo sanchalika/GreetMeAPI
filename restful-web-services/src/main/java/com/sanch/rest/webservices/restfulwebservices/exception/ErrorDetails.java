976076070acd21132d405c5cb5710457b9858db0
Sanchalika <sanchalika2019@gmail.com>
2023-06-07 16:12:49 +0530

    Add files via upload

---
+++ restful-web-services/src/main/java/com/sanch/rest/webservices/restfulwebservices/exception/ErrorDetails.java
@@ -0,0 +1,32 @@
+package com.sanch.rest.webservices.restfulwebservices.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	//timestamp
+	//error message
+	//details
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+	
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	
+	
+	
+}
